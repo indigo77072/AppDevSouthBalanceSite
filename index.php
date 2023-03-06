89421ac905bb3bafe796acb427486f095052dfe8
indigo_emanuel <indigo.emanuel@gmail.com>
2023-03-05 19:38:00 -0600

    added sql connection statment

--- index.php
+++ index.php
@@ -6,7 +6,22 @@
 
 <?php
 echo "Hello World!";
+
+$servername = "localhost";
+$username = "student";
+$password = "SoPAStudentDB!#!";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
 ?>
+
 
 </body>
 </html>
