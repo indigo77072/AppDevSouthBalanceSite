9c1e6192a06878cc6bc90cd70c24ff747087fd23
Indigo Emanuel <indigo.emanuel@gmail.com>
2023-03-06 19:31:37 -0600

    Update index.php
    Edited some of the code to reflect the database

--- index.php
+++ index.php
@@ -10,38 +10,40 @@ echo "Hello World!";
 $servername = "localhost";
 $username = "student";
 $password = "SoPAStudentDB!#!";
+$database = "south_balance_database";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $database);
 
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+  echo "Connected successfully!!!!";
 
 //Reading data
-$sql = "SELECT id, firstname, lastname FROM MyGuests";
+$sql = "SELECT * FROM accounts";
+//$sql = "SELECT Quantity > 0 FROM inventory_items WHERE item_id = 1 ";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    echo "Username: " . $row["Username"]. " - Name: " . $row["Firstname"]. " " . $row["Lastname"]. "<br>";
   }
 } else {
   echo "0 results";
 }
 
 //Saving - Inserting data into the database
-$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+/*$sql = "INSERT INTO MyGuests (firstname, lastname, email)
 VALUES ('John', 'Doe', 'john@example.com')";
 
 if ($conn->query($sql) === TRUE) {
   echo "New record created successfully";
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
-}
+}*/
 ?>
 
 
