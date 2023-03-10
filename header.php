c1e2144a725ed87c2aa0da3e83abfa0bd4ae9256
williamshields2 <37985502+williamshields2@users.noreply.github.com>
2023-03-10 17:05:38 -0600

    Place logo and simplify index.php

--- header.php
+++ header.php
@@ -1,8 +1,8 @@
 <header id="topHeader">
             <div id="leftSection">
-                <p id="logoImgPlaceholder">Logo</p>
+                <img id="logoImg" src="graphics/sb_logo.png" alt="South Balance logo">
             </div><div id="centerSection">
-                <h1 id="siteTitle">South Balance Online Store </h1>
+                <h1 id="siteTitle">South Balance Online Store</h1>
             </div><div id="rightSection">
                 <p id="navigation">Home | About us | Catalog | Cart | Support | Logout</p>
             </div>
--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <!DOCTYPE html>
 <html>
+
+<head>
+        <meta charset="UTF-8"></meta>
+        <link rel="stylesheet" href="header_styles.css">
+        <link rel="stylesheet" href="index_styles.css">
+        <title>South Balance - Home</title>
+</head>
 <body>
 
-<h1>My first PHP page</h1>
+<?php include 'header.php' ?>
 
 <?php
-echo "Hello World!";
 
 $servername = "vdi-sopadb-p40";
 $username = "student";
@@ -14,39 +20,45 @@ $password = "SoPAStudentDB!#!";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
-
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}else{
-  echo "Connected successfully!!!!";
-}
-
-
-//Reading data
-$sql = "SELECT * FROM south_balance_database.accounts"; // WE ARE GETTING THE ERROR THAT THIS TABLE DOES NOT EXIST??
-//$sql = "SELECT Quantity > 0 FROM inventory_items WHERE item_id = 1 "; Will's Code
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-    echo "Username: " . $row["Username"]. " - Name: " . $row["First_Name"]. " " . $row["Last_Name"]. "<br>";
-  }
-} else {
-  echo "0 results";
-}
-
-//Saving - Inserting data into the database
-/*$sql2 = "INSERT INTO accounts (Username, Password, First_Name, Last_Name)
-VALUES ('BBilly2', 'ItsASecret','Bob', 'Billy)";
-
-if ($conn->query($sql2) === TRUE) {
-  echo "New record created successfully";
-} else {
-  echo "Error: " . $sql2 . "<br>" . $conn->error;
-}*/
 ?>
+
+  <div id="searchBar">
+    <input type="text" id="search">
+</div>
+
+<!--
+// // Check connection
+// if ($conn->connect_error) {
+//   die("Connection failed: " . $conn->connect_error);
+// }else{
+//   echo "Connected successfully!!!!";
+// }
+
+
+// //Reading data
+// $sql = "SELECT * FROM south_balance_database.accounts"; // WE ARE GETTING THE ERROR THAT THIS TABLE DOES NOT EXIST??
+// //$sql = "SELECT Quantity > 0 FROM inventory_items WHERE item_id = 1 "; Will's Code
+// $result = $conn->query($sql);
+
+// if ($result->num_rows > 0) {
+//   // output data of each row
+//   while($row = $result->fetch_assoc()) {
+//     echo "Username: " . $row["Username"]. " - Name: " . $row["First_Name"]. " " . $row["Last_Name"]. "<br>";
+//   }
+// } else {
+//   echo "0 results";
+// }
+
+// //Saving - Inserting data into the database
+// /*$sql2 = "INSERT INTO accounts (Username, Password, First_Name, Last_Name)
+// VALUES ('BBilly2', 'ItsASecret','Bob', 'Billy)";
+
+// if ($conn->query($sql2) === TRUE) {
+//   echo "New record created successfully";
+// } else {
+//   echo "Error: " . $sql2 . "<br>" . $conn->error;
+// }*/
+// -->
 
 
 </body>
