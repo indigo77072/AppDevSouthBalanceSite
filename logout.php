9251698f1c92530d72cf00445b15a4c142f31d5f
williamshields2 <37985502+williamshields2@users.noreply.github.com>
2023-03-11 17:29:46 -0600

    Update logout.php

--- logout.php
+++ logout.php
@@ -11,14 +11,21 @@
         <link rel="stylesheet" href="header_styles.css">
         <link rel="stylesheet" href="logout_styles.css">
         <title>South Balance - Logout</title>
-        <!-- Will's guess: an external logout script (PHP) will run when this webpage is loaded. -->
+        <!-- Will's guess: a logout script (PHP) will run when this webpage is loaded. -->
         
     </head>
     <body>
 
-        <?php include 'header.php' ?>
-
-        <p id="logoutMsg">You have successfully logged out.</p>
+        <?php include 'header.php'
+        // logout code adapted from:
+        // https://www.oreilly.com/library/view/learning-php-mysql/9781449337452/ch21s14.html#:~:text=php%2C%20the%20logout%20page%20that,the%20top%20of%20the%20screen.
+            if (isset($_SESSION['user']))
+            {
+                destroySession();
+                echo "<p id=\"logoutMsg\">You have successfully logged out.</p>";
+            }
+            else echo "<p id=\"logoutMsg\">You cannot log out because you are not logged in.</p>";
+        ?>
 
     </body>
 
