8e53583027c45179ed3451c0dfbee5efea3b3d07
hector-022b <127160503+hector-022b@users.noreply.github.com>
2023-03-11 22:48:33 -0600

    Create Login.php

---
+++ Login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Login Page</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+  <div class="loginbox">
+    <h1>Login</h1>
+    <form method="POST" action="login.php">
+      <p>Username</p>
+      <input type="text" name="username" placeholder="Enter Username">
+      <p>Password</p>
+      <input type="password" name="password" placeholder="Enter Password">
+      <input type="submit" name="submit" value="Login">
+    </form>
+  </div>
+</body>
+
+</html>
