c2a7df4d5bdf3c62920debcf6b763f7f602627de
hector-022b <127160503+hector-022b@users.noreply.github.com>
2023-04-17 16:55:24 -0500

    Update login.php

--- login.php
+++ login.php
@@ -30,7 +30,7 @@ if(isset($_GET["msg2"])){
 <body>
   <div class="left-image"></div>
   <div class="right-image"></div>
-  <h1 class="title"><span class="SignIn">Sign In</span> To The South Balance Online Store</h1>
+  <h1>Sign In To The South Balance Online Store</h1>
   <div class="loginbox">
     <h2>Login</h2>
     <form method="POST" action="login_Process.php">
