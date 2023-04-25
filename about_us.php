4815db8bd59178799256f8a3f674e33946373bc0
hector-022b <127160503+hector-022b@users.noreply.github.com>
2023-04-25 17:31:50 -0500

    Update about_us.php

--- about_us.php
+++ about_us.php
@@ -16,20 +16,37 @@
 
         <?php include 'header.php' ?>
 
-        <p id="missionStatement">Our Mission: At South Balance, Inc., we never stop innovating.<br>
-            It takes hundreds of passionate, dedicated, and talented<br>
-            employees around the world to deliver the merchandise and<br>
-            shopping experience our customers expect and deserve.</p>
+        <section id="about">
+            <h2>Misson:</h2>
+            <p class="missionStatement">Our Mission: At South Balance, Inc., we never stop innovating.<br>
+                It takes hundreds of passionate, dedicated, and talented<br>
+                employees around the world to deliver the merchandise and<br>
+                shopping experience our customers expect and deserve.</p>
+
+		<section id="theCompany">
+			<h2>The Company:</h2>
+            <img src="graphics/sb_emp.png" alt="South Balance Employees" style="float:right;margin-left:10px;width:550px;height:400px;">
+            <p class="bodyContent">At South Balance Inc., we never stop innovating. It takes hundreds of 
+                passionate, dedicated, and talented employees around the world to deliver the merchandise and 
+                shopping experience our customers expect and deserve. South Balance was able to successfully expand 
+                to Europe five years ago, as they continue to grow that line of business an opportunity arose where one 
+                of the company’s executives who is ex-military (USAF) with ties to the Army & Air Force Exchange Service 
+                (AAFES, also referred to as The Exchange and The PX or the BX) is the retailer in U.S. Army and Air Force 
+                installations worldwide.</p>
+		</section>
         
         <footer>
             <p>South Balance Social Media</p>
-            <div id="socialMediaIcons">
-                <img id="facebook" src="graphics/facebook.webp">
-                <img id="twitter" src="graphics/twitter.png">
-                <img id="instagram" src="graphics/instagram.webp">
-                <img id="linkedin" src="graphics/linkedin.webp">
-                <img id="youtube" src="graphics/youtube.png">
-            </div>
-        </footer>
+                <div id="socialMediaIcons">
+                    <img id="facebook" src="graphics/facebook.webp">
+                    <img id="twitter" src="graphics/twitter.png">
+                    <img id="instagram" src="graphics/instagram.webp">
+                    <img id="linkedin" src="graphics/linkedin.webp">
+                    <img id="youtube" src="graphics/youtube.png">
+                </div>
+        <p>© 2023 Copyright & Powered By Tulane Tech -
+          Tulane School of Professional Development
+        </p>
+    </footer>
     </body>
 </html>
